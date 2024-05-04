341e67304c0d89e8c8d3c45f3d6cc6cae9ee6c65
NadavIsraeli <163755376+HarbringerOfDoom@users.noreply.github.com>
2024-05-04 18:48:06 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,41 +1,32 @@
 import socket
-from _thread import *
+import _thread
 import json
 import math
+from settings import *
 
-VIEW_SIZE = (500, 500)
-MAP_SIZE = (2000, 2000)
-
-def sendDataAndInter(conn, pelletsList, virusList, spawnersList, playerToClient, clientToPlayers, playersList):
-    for pellet in pelletsList:
-        if not checkInter(conn, clientToPlayers, playerToClient, pellet, pelletsList):
-            if broadcast(pellet.to_json(), conn) == -1:
-                return -1
-
-    for virus in virusList:
-        if not checkInter(conn, clientToPlayers, playerToClient, virus, virusList):
-            if broadcast(virus.to_json(), conn) == -1:
-                return -1
-
-    for spawner in spawnersList:
-        if not checkInter(conn, clientToPlayers, playerToClient, spawner, spawnersList):
-            if broadcast(spawner.to_json(), conn) == -1:
-                return -1
+def sendDataAndInter(conn, addr, entetyList, clientToPlayer, playersList):
+    sendingData = []
+    for entety in entetyList:
+        if not checkInter(addr, clientToPlayer, entety, entetyList, playersList):
+            sendingData.append(entety)
     
-    for player in playersList:
-        if not checkInter(conn, clientToPlayers, playerToClient, player, playersList, True):
-           if broadcast(player.to_json(), conn) == -1:
-                return -1
+    for blob in playersList:
+        if not checkInter(addr, clientToPlayer, blob, playersList, True):
+            sendingData.append(blob)
 
-def checkInter(conn, clientToPlayers, playerToClient, entety, entetyList, playerType = False):
-    for player in clientToPlayers[conn]:
-        if entety.eaten(player):
+    broadcast(json.dumps(sendingData), conn)
+
+def checkInter(addr, clientToPlayer, entety, entetyList, playerType = False):
+    player = clientToPlayer[addr[0]]
+    if entety.eaten(player):
+        entetyList.remove(entety)
+        if playerType:
+            addr = entety.addr
+            clientToPlayer.pop(addr[0], entety)
+            
+        else:
             entetyList.remove(entety)
-            if playerType:
-                client = playerToClient[entety]
-                clientToPlayers[client].remove(entety)
-                playerToClient.pop(entety, None)
-            return True
+        return True
     return False
 
 def broadcast(message, conn):
@@ -45,56 +36,51 @@ def broadcast(message, conn):
         conn.close()
         return -1
 
-def clientthread(conn, pelletsList, virusList, spawnersList, playerToClient, clientToPlayers, playersList):
+def clientthread(conn, addr, listOfClients, entetyList, clientToPlayer, playersList):
     while True:
-        if sendDataAndInter(conn, pelletsList, virusList, spawnersList, playerToClient, clientToPlayers, playersList) == -1:
+        if sendDataAndInter(conn, entetyList, clientToPlayer, playersList) == -1:
+            player = clientToPlayer[addr[0]]
+            clientToPlayer.pop(addr[0], player)
+            listOfClients.remove(conn)
+            playersList.remove(player)
+            conn.close()
             return -1
 
-def move(conn, playerToClient, clientToPlayers, playersList):
+def move(conn, addr, clientToPlayer, playersList):
     pos = json.load(conn.recv(1024).decode())
-    middleScreen = [0, 0]
-    for player in clientToPlayers[conn]:
-        middleScreen[0] += player.x
-        middleScreen[1] += player.y
-    middleScreen[0] /= len(clientToPlayers[conn])
-    middleScreen[1] /= len(clientToPlayers[conn])
-    for blob in clientToPlayers[conn]:
-        y = blob.y - middleScreen[1]
-        x = blob.x - middleScreen[0]
-        angle = math.atan2(pos[1] - VIEW_SIZE[1] / 2 - y, VIEW_SIZE[0] / 2 - pos[0] - x)
-        clientToPlayers[conn].remove(blob)
-        playerToClient.pop(blob, None)
-        playersList.remove(blob)
-        blob.move(angle, MAP_SIZE)
-        clientToPlayers[conn].append(blob)
-        playerToClient[blob] = conn
-        playersList.append(blob)
+    middleScreen = (clientToPlayer[addr[0]].x, clientToPlayer[addr[0]].y)
+
+    player = clientToPlayer[addr[0]]
+    y = player.y - middleScreen[1]
+    x = player.x - middleScreen[0]
+    angle = math.atan2(VIEW_SIZE[1] / 2 - pos[1] - y, VIEW_SIZE[0] / 2 - pos[0] - x)
+    clientToPlayer.pop(addr[0], player)
+    playersList.remove(player)
+    if (pos[0] - x) ** 2 + (pos[1] - y) ** 2 > player.size ** 2:
+        player.move(angle, MAP_SIZE)
+    clientToPlayer[addr[0]] = player
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    IP_address = "127.0.0.1"
-    Port = 8080
-
-    server.bind((IP_address, Port))
+    server.bind((IP_ADDRESS, PORT))
     server.listen(10)
     list_of_clients = []
 
-    pelletsList = []
-    virusList = []
-    spawnersList = []
-    playerToClient = {}
-    clientToPlayers = {}
-    playersList = []
+    entetyList = {}
+    clientToPlayer = {}
+    playersList = {}
 
     while True:
         conn, addr = server.accept()
         list_of_clients.append(conn)
         print (addr[0] + " connected")
-        start_new_thread(clientthread,(conn, pelletsList, virusList, spawnersList, playerToClient, clientToPlayers, playersList))
+        _thread.start_new_thread(clientthread,(conn, entetyList, clientToPlayer, playersList))
 
-    conn.close()
+    for conn in list_of_clients:
+        conn.close()
+
     server.close()
 
 main()
